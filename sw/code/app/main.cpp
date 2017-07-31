b466fc1e7e1517f3b7e3a90f07639b7ed5adfa8b
Murat Ozkan <murat.ozkan946@gmail.com>
2017-07-30 20:42:37 -0700

    Add gamma correction to pixels
    Added gamma correction for pixel setters. Looks better now on linear
    brightness sweep test.

--- sw/code/app/main.cpp
+++ sw/code/app/main.cpp
@@ -30,17 +30,86 @@ int main(void) {
       std::vector<hal::APA102Pixel>(25, hal::APA102Pixel(1, 3, 7, 5)),
     }};
   
-  all_strings[0][1] = hal::APA102Pixel(4, 5, 6, 15);
-  all_strings[1][2] = hal::APA102Pixel(4, 5, 6, 15);
-  all_strings[2][3] = hal::APA102Pixel(4, 5, 6, 15);
-  all_strings[3][4] = hal::APA102Pixel(4, 5, 6, 15);
-  all_strings[4][5] = hal::APA102Pixel(4, 5, 6, 15);
-  all_strings[5][6] = hal::APA102Pixel(4, 5, 6, 15);
+#if 0
+  hal::Timing& timing = dress.GetTiming();
 
-  while(1) {
-    //display.Update(single_string);
+  uint32_t last_millis = timing.Millis();
+  uint32_t poll_time = timing.Millis();
+
+  while (1) {
+    while ((poll_time - last_millis) < 1000) {
+      poll_time = timing.Millis();
+    }
+    last_millis = poll_time;
+    all_strings[0][0].set_r(255);
+    all_strings[0][0].set_g(255);
+    all_strings[0][0].set_b(255);
+    display.Update(all_strings);
+
+    while ((poll_time - last_millis) < 1000) {
+      poll_time = timing.Millis();
+    }
+    last_millis = poll_time;
+    all_strings[0][0].set_r(0);
+    all_strings[0][0].set_g(0);
+    all_strings[0][0].set_b(0);
     display.Update(all_strings);
   }
+#endif
+
+#if 0
+  while(1) {
+    //display.Update(single_string);
+    for (int string = 0; string < 6; string++) {
+      for (int i = 0; i < 25; i++) {
+        all_strings[string][i] = hal::APA102Pixel(100, 5, 6, 31);
+        //all_strings[string][i] = hal::APA102Pixel(186, 37, 148, 31);
+        //all_strings[string][i] = hal::APA102Pixel(255, 246, 173, 31);
+
+        display.Update(all_strings);
+        //for (int i = 0; i < 10000; i++) { }
+      }
+    }
+
+    for (int string = 0; string < 6; string++) {
+      for (int i = 0; i < 25; i++) {
+        all_strings[string][i] = hal::APA102Pixel(10, 5, 5, 31);
+        all_strings[string][i] = hal::APA102Pixel(5, 10, 5, 31);
+        all_strings[string][i] = hal::APA102Pixel(5, 5, 10, 31);
+        all_strings[string][i] = hal::APA102Pixel(10, 10, 5, 31);
+        all_strings[string][i] = hal::APA102Pixel(5, 10, 10, 31);
+        all_strings[string][i] = hal::APA102Pixel(10, 5, 10, 31);
+
+        display.Update(all_strings);
+        for (int i = 0; i < 10000; i++) { }
+      }
+    }
+  }
+#endif
+
+#if 1
+  uint8_t count = 0;
+  int8_t  direction = 1;
+
+  while (1) {
+    if (count == 255) {
+      direction = -1;
+    } else if (count == 0) {
+      direction = 1;
+    }
+
+    for (int string = 0; string < 6; string++) {
+      for (int i = 0; i < 25; i++) {
+        all_strings[string][i].set_r(count);
+        all_strings[string][i].set_g(count);
+        all_strings[string][i].set_b(count);
+      }
+    }
+
+    display.Update(all_strings);
+    count += direction;
+  }
+#endif
 
   return 0;
 }
--- sw/code/hal/inc/APA102Pixel.h
+++ sw/code/hal/inc/APA102Pixel.h
@@ -56,31 +56,15 @@ class APA102Pixel
 
   ~APA102Pixel();
 
-  void set_r(uint8_t r) { r_ = r; }
-  void set_g(uint8_t g) { g_ = g; }
-  void set_b(uint8_t b) { b_ = b; }
-  void set_bright(uint8_t bright) { bright_ = bright; }
+  void set_r(uint8_t r);
+  void set_g(uint8_t g);
+  void set_b(uint8_t b);
+  void set_bright(uint8_t bright);
 
   uint8_t r(void) const { return r_; }
   uint8_t g(void) const { return g_; }
   uint8_t b(void) const { return b_; }
   uint8_t bright(void) const { return bright_; }
-
-  /**
-   * (NOT TESTED)
-   * Gets the four-byte raw packed representation for shifting out to the LED
-   * string.
-   *
-   * Note: This may be useful for when DMA based SPI transfers are implemented.
-   *
-   * @return Raw representation of LED data to send over the wire.
-   */
-  uint32_t GetRaw(void) {
-    return (static_cast<uint32_t>(bright_ | 0xE0) << 24) |
-           (static_cast<uint32_t>(b_) << 16) |
-           (static_cast<uint32_t>(g_) << 8) |
-           static_cast<uint32_t>(r_);
-  }
 
  private:
   uint8_t r_;
